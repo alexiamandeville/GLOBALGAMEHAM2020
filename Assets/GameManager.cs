0b76eedb6b2439426138750e8b0ded9b20253b11
Alexia Mandeville <flexmandeville@gmail.com>
2020-01-31 21:03:55 -0800

    Scores, GameManager, Canvas
    Score prefab
    GameManager prefab
    Canvas prefab

---
+++ Assets/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    float timeLeft = 180.0f;
+
+    public Text timeText;
+    public Text score;
+    public ScoreSystem myScore;
+
+    private float possibleEvents = 20f;
+
+    void Update()
+    {
+
+        timeLeft -= Time.deltaTime ;
+
+        timeText.text = "Time " + timeLeft / 60f;
+
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+
+        TallyScore();
+
+    }
+
+    void TallyScore()
+    {   
+        score.text = Mathf.Clamp(((myScore.playerScore / possibleEvents) * 100), 1, 100).ToString();
+    }
+
+    void GameOver()
+    {
+        timeText.text = "Time's up";
+    }
+
+}
---
+++ Assets/ScoreSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public float playerScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPoint();
+        ghostPoint();
+    }
+
+    void playerPoint()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            playerScore += 1f;
+        }
+    }
+
+    void ghostPoint()
+    {
+        if (Input.GetKeyDown("g"))
+        {
+            print("ddd");
+            playerScore -= 1f;
+        }
+    }
+}
