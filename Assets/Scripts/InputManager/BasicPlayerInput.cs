b0b1be52073f050849f7ca132913a4815bad8482
Hardeep Singh <ootz0rz@gmail.com>
2020-01-31 22:18:39 -0800

    layers

--- Assets/Scripts/InputManager/BasicPlayerInput.cs
+++ Assets/Scripts/InputManager/BasicPlayerInput.cs
@@ -9,12 +9,74 @@ namespace DefaultNamespace
     [SerializeField] protected Rigidbody rb;
     [SerializeField] protected int controllerNumber = 0;
     [SerializeField] protected float moveSpeedMulti;
+    [SerializeField] protected BoxCollider playerGroundCollider;
 
     protected Gamepad currentGamepad;
 
-    protected void Awake()
+    public virtual void Awake()
     {
       currentGamepad = Gamepad.all[controllerNumber];
+    }
+    
+    public virtual void FixedUpdate()
+    {
+      UpdateCharacterPosition();
+    }
+
+    protected bool IsMovementEnabled()
+    {
+      if (currentGamepad == null || !currentGamepad.enabled)
+      {
+        return false;
+      }
+
+      if (playerRootTransform == null)
+      {
+        return false;
+      }
+
+      return true;
+    }
+
+    protected void UpdateCharacterPosition()
+    {
+      if (!IsMovementEnabled())
+      {
+        return;
+      }
+      
+      // initial pos
+      var curP = rb.transform.position;
+      var newX = curP.x;
+      var newY = curP.y;
+      var newZ = curP.z;
+      
+      // check for left thumbstick to move around with
+      var moveAxis = currentGamepad.leftStick.ReadValue();
+      if (moveAxis.magnitude > 0)
+      {
+        var multi = Time.fixedDeltaTime * moveSpeedMulti;
+
+        newX -= (moveAxis.x * multi);
+        newZ -= (moveAxis.y * multi);
+      }
+
+      // make sure we're properly grounded
+      var didHit = Physics.CheckSphere(
+        playerGroundCollider.center,
+        -.5f, layerMask: Layers.GroundMask, QueryTriggerInteraction.Collide);
+      if (!didHit)
+      {
+        // add ze gravitas
+        newY -= (0.1f);
+      }
+      else
+      {
+        Debug.Log("ON GROUND");
+      }
+      
+      // apply final movement
+      rb.MovePosition(new Vector3(newX, newY, newZ));
     }
   }
 }
--- Assets/Scripts/InputManager/FlipperPlayerInput.cs
+++ Assets/Scripts/InputManager/FlipperPlayerInput.cs
@@ -5,28 +5,6 @@ namespace DefaultNamespace
 {
   public class FlipperPlayerInput : BasicPlayerInput
   {
-    protected void Update()
-    {
-      if (currentGamepad == null || !currentGamepad.enabled)
-      {
-        return;
-      }
 
-      if (playerRootTransform == null)
-      {
-        return;
-      }
-      
-      // check for left thumbstick to move around with
-      var moveAxis = currentGamepad.leftStick.ReadValue();
-      
-      // move in direction of thumbstick
-      var curP = rb.transform.position;
-      rb.MovePosition(new Vector3(
-        curP.x - (moveAxis.x * moveSpeedMulti),
-        curP.y,
-        curP.z - (moveAxis.y * moveSpeedMulti)
-      ));
-    }
   }
 }
