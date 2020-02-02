69c8db5bf46da48312b4a14950224799b81db3f8
Alexia Mandeville <flexmandeville@gmail.com>
2020-02-01 16:53:42 -0800

    Game Start
    Game start screen
    Artwork
    Text
    Goes to main scene

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    public GameObject myStartUI;
+
+    void Start()
+    {
+        myStartUI.SetActive(false);
+        StartCoroutine(StartGame());
+    }
+
+    IEnumerator StartGame()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(16f);
+
+            myStartUI.SetActive(true);
+
+            if(Input.GetKeyDown("joystick button 0"))
+                SceneManager.LoadScene("main");
+
+        }
+    }
+}
