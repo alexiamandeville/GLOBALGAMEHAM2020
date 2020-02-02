7e1b16690f2e8a12d6fe4402bcaeaa88c96f2f99
Andrew <drew.l.connell@gmail.com>
2020-02-01 20:46:45 -0500

    Adding maid animation, and slime

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DefaultNamespace;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator = null;
+    private PlayerInput playerInput = null;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerInput = GetComponentInParent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        WalkAnimation();
+    }
+
+    private void WalkAnimation()
+    {
+        Vector2 moveAxis = playerInput.currentGamepad.leftStick.ReadValue();
+        bool isWalking = moveAxis == Vector2.zero ? false : true;
+        animator.SetBool("Walk", isWalking);
+    }
+}
