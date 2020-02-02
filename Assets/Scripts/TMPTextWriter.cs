965f983eb8031a307c0c5581981a066b3b06cd77
Hardeep Singh <ootz0rz@gmail.com>
2020-02-02 00:08:56 -0800

    higher quality intro scene text w/ TMP

---
+++ Assets/Scripts/TMPTextWriter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+  public class TMPTextWriter : MonoBehaviour
+  {
+
+    TextMeshProUGUI txt;
+    string story;
+
+    void Awake()
+    {
+      txt = GetComponent<TextMeshProUGUI>();
+      story = txt.text;
+      txt.text = "";
+
+      // TODO: add optional delay when to start
+      StartCoroutine("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+      foreach (char c in story)
+      {
+        txt.text += c;
+        yield return new WaitForSeconds(0.05f);
+      }
+    }
+  }
+}
